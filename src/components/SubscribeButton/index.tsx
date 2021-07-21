02ca66a4951e2832ec5455cc301d6f4e75713698
unknown <fernandoalvesq@gmail.com>
2021-07-21 19:26:19 -0300

    criado toda a comunicao com o stripe para que seja criado o usuario no stripe e gravado esse usuario no banco para que na proxima vez que entrar nao seja duplicado o cadastro no strpe, tivemos que instalar a dependencia do stripe.js

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe.js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,11 +8,34 @@ interface SubscribeButtonProps {
 }
 
 
-export function SubscribeButton (props: SubscribeButtonProps) {
+export function SubscribeButton ({ priceId }: SubscribeButtonProps) {
+  async function hundleSubscribe() {
+    const [session] = useSession();
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    //criacao da checkout session 
+    try{ 
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({sessionId : sessionId});
+    } catch(err) {
+      alert(err.message);
+    }
+
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={hundleSubscribe}
     >
       Subscribe now
     </button>
