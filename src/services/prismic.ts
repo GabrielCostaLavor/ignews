51204b14f44ebb83f13f439ba89537b18ef00ff2
unknown <fernandoalvesq@gmail.com>
2021-07-22 19:41:01 -0300

    criacao do servise CMS Prismic, a pagina posts ja esta coletando as informacoes do que esta salvo no prismic

---
+++ src/services/prismic.ts
@@ -0,0 +1,13 @@
+import Prismic from '@prismicio/client';
+
+export function getPrismicClient (req?: unknown) {
+  const prismic = Prismic.client(
+    process.env.PRISMIC_ACESS_URL,
+    {
+      req,
+      accessToken:process.env.PRISMIC_ACESS_KEY
+    }
+  );
+
+  return prismic;
+} 
